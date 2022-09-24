2cb57abcb1e884b74bc0c0ac43c068da026062fe
Will Ozeas <willatme@me.com>
2022-09-24 14:58:23 -0400

    Room struct

---
+++ RoomParser.cpp
@@ -0,0 +1,8 @@
+#include "RoomParser.hpp"
+#include "data_path.hpp"
+#include <iostream>
+
+Room RoomParser::parse_room(std::string path) {
+    Room room;
+    std::istream room_file();
+}
---
+++ RoomParser.hpp
@@ -0,0 +1,22 @@
+
+
+#ifndef INC_15_466_F22_GAME4_ROOMPARSER_HPP
+#define INC_15_466_F22_GAME4_ROOMPARSER_HPP
+#include <glm/glm.hpp>
+#include <vector>
+#include <string>
+#include <map>
+
+struct Room {
+    std::string main_text;
+    std::vector<std::string> choices;
+    std::map<std::string, std::string> inputs;
+};
+
+struct RoomParser {
+    RoomParser();
+
+    Room parse_room(std::string);
+};
+
+#endif //INC_15_466_F22_GAME4_ROOMPARSER_HPP
