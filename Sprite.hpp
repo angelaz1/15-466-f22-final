c4db26c461597d6db425a61bd650880ac46c96f4
Angela Zhang <angelaz1@andrew.cmu.edu>
2022-10-30 13:45:55 -0400

    Modifications to arrow UI display

--- Sprite.hpp
+++ Sprite.hpp
@@ -27,7 +27,13 @@ struct Sprite {
     std::vector< glm::u8vec4 > data;
     void set_drawable_size(const glm::uvec2 &new_drawable_size);
 
-    void draw(glm::vec2 center_pos, float scale);
+    /** 
+     * Draws the current sprite 
+     * center_pos - position for the center of the sprite
+     * scale - factor to scale the sprite by, 1.0 for original scale
+     * hue - color hue of the sprite in RGBA, use white (all 0xff) for original color
+     **/
+    void draw(glm::vec2 center_pos, float scale = 1.0f, glm::u8vec4 hue = glm::u8vec4(0xff));
     
     glm::mat4 world_to_clip;
 
@@ -39,7 +45,7 @@ struct Sprite {
 		glm::vec2 TexCoord;
 	};
 
-	// Based on DrawLiens.cpp
+	// Based on DrawLines.cpp
 	GLuint vertex_buffer = 0;
 	GLuint vertex_buffer_for_color_texture_program = 0;
 
